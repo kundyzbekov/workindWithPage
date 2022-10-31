97d443086565ce395f806c085d549404715cfcd0
Еркебулан Кундызбеков <erkebulankundyzbekov@MacBook-Air-Erkebulan.local>
2022-10-31 10:51:42 +0600

    practiced with project, processing action on page

--- js/script.js
+++ js/script.js
@@ -24,3 +24,26 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img');
+const poster = document.querySelector('.promo__bg');
+const genre = poster.querySelector('.promo__genre');
+const movieList = document.querySelector('.promo__interactive-list');
+// adv.forEach(i=>{
+//     i.remove();
+// });
+
+genre.textContent = 'Драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((films, i) =>{
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i+1} ${films}
+    <div class="delete"></div>
+    </li>
+    `
+});
